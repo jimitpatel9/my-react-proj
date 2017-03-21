afdf9a598e8b5fca6d245ff2bede24fe60b9a730
ADC7BDQ <jimit.patel@bnymellon.com>
2017-03-21 17:04:32 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -1,50 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import CSSModules from 'react-css-modules';
-import styles from './file.css';
+import styles from './app.css';
 
 const GIT_URL_FOR_USER_DETAILS = 'https://api.github.com/users/';
 
 class App extends React.Component {
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      userName:'',
-      dataKeys: [],
-      data: ''
-    };
+        this.state = {
+            userName: '',
+            userDataKeys: [],
+            userData: ''
+        };
 
-    this.update = this.update.bind(this);
-    this.click = this.click.bind(this);
-  }
+        this.update = this.update.bind(this);
+        this.getGithubInfo = this.getGithubInfo.bind(this);
+    }
 
-  update(e) {
-    this.setState({userName: e.target.value})
-  }
+    /**
+     * Gets the username from input box and updates username state
+     * @param {Object} e event
+     */
+    update(e) {
+        this.setState({userName: e.target.value})
+    }
 
-  click() {
-    fetch(GIT_URL_FOR_USER_DETAILS + this.state.userName)
-        .then((response) => response.json())
-        .then((userDetails) => this.setState({dataKeys: Object.keys(userDetails),data: userDetails}))
-  }
+    /**
+     * Fetches the user details from github api
+     */
+    getGithubInfo() {
+        fetch(GIT_URL_FOR_USER_DETAILS + this.state.userName)
+            .then((response) => response.json())
+            .then((userDetails) => this.setState({userDataKeys: Object.keys(userDetails), userData: userDetails}))
+    }
 
-  render() {
-    return (
-        <div>
-          <h1>Learn React-Webpack-CSSModules</h1>
-          <input type="text"
-                 onChange={this.update}/>
-          <button onClick={this.click}>Search</button>
-          <p>{this.state.userName}</p>
-            <ul>
-                {this.state.dataKeys.map((listValue) => {
-                    return <li>{listValue} : {this.state.data[listValue]}</li>;
-                })}
-            </ul>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1 styleName="text-center">Learning React-Webpack-CSSModules</h1>
+                <label styleName="block-with-margin-lr" htmlFor="username">Enter Github Username</label>
+                <input type="text"
+                       onChange={this.update}
+                       id="username"
+                       styleName="margin-lr-10" />
+                <button styleName="margin-lr-10"
+                        onClick={this.getGithubInfo}>Get Info</button>
+                <ul styleName="no-style-and-padding">
+                    {this.state.userDataKeys.map((key) => {
+                        return <li key={key}>{key} : {this.state.userData[key]}</li>;
+                    })}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default CSSModules(App, styles);
